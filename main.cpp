73780296ac1e389141044b36d67cf656d5715e9d
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-07-07 14:06:38 -0500

    Se agregó la librería Errores.h

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include "./Librerias/CrearTokens.h"
+#include "./Librerias/Errores.h"
 
 using namespace std;
 
 int main() {
     CrearTokens tokens;
+    Errores err;
     string tabla_tokens="TablaTokens.txt", token_error="TablaErrores.txt", archivo_tokens="Tokens.txt";
 //  Creación de los archivos necesarios para guardar los resultados
     tokens.crearArchivo(tabla_tokens);
     tokens.crearArchivo(token_error);
     tokens.crearArchivo(archivo_tokens);
+
+    err.detectarErrores();
 // Función que llama a todas las otras funciones que analizan los lexemas y escriben los resultados en el archivo TablaTokens.txt
     tokens.analizarLexemas();
 
